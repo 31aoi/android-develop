65f87fc6dd47ae45a93aa158d9b2680213b4631d
Dashboard <2111393715@qq.com>
2024-09-25 00:27:40 +0800

    实验二

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,6 @@
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.test4);
         }
     }
